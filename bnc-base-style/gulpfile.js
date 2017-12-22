07633081a7595900346e485bc8604b8e38416f0a
BulletYuan <326583424@qq.com>
2017-12-22 11:31:39 +0800

    update style file

--- bnc-base-style/gulpfile.js
+++ bnc-base-style/gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('less', function () {
 // 在命令行使用 gulp auto 启动此任务
 gulp.task('auto', function () {
     // 监听文件修改，当文件被修改则执行 less 任务
-    gulp.watch('less/**.less', ['less'])
+    gulp.watch('**.less', ['less'])
 })
 
 // 使用 gulp.task('default') 定义默认任务
